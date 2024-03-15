54ffb41d454360893401bd52ccd19807297d78b9
Kape99 <lucapecchi13@gmail.com>
2024-03-15 19:29:32 +0100

    lily58 fix

--- keyboards/lily58/keymaps/default/config.h
+++ keyboards/lily58/keymaps/default/config.h
@@ -26,6 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define SPLIT_USB_DETECT
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
 
